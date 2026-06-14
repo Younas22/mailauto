89be0566639aaa9492307532aeb64858bb29343d
Younas22 <hm.younas22@gmail.com>
2026-06-14 13:17:12 +0500

    i

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '/webhooks/ses',
             '/webhooks/resend',
+            '/webhooks/mailgun-reply',
         ]);
 
         $middleware->web(append: [
